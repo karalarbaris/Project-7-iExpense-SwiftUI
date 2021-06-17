e8012ad8add349d372439f4fc125ebdb39c2c04f
Baris <karalarbaris@gmail.com>
2021-06-18 00:32:14 +0200

    Showing and hiding views

--- Project-7-iExpense-SwiftUI/ContentView.swift
+++ Project-7-iExpense-SwiftUI/ContentView.swift
@@ -7,29 +7,63 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Baris"
-    @Published var lastName = "Karalar"
+struct SecondView: View {
+    
+    var name: String
+    @Environment(\.presentationMode) var presentatiionMode
+    
+    var body: some View {
+        Text("Second view \(name)")
+
+        Button("Dismiss") {
+            self.presentatiionMode.wrappedValue.dismiss()
+        }
+    }
 }
 
 struct ContentView: View {
     
-    @ObservedObject var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
         
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            
+        Button("Show sheet") {
+            showingSheet.toggle()
         }
+        .sheet(isPresented: $showingSheet, content: {
+            SecondView(name: "Barisss")
+        })
         
 
     
     }
 }
+
+////Sharing SwiftUI state with @ObservedObject
+//
+//class User: ObservableObject {
+//    @Published var firstName = "Baris"
+//    @Published var lastName = "Karalar"
+//}
+//
+//struct ContentView: View {
+//
+//    @ObservedObject var user = User()
+//
+//    var body: some View {
+//
+//        VStack {
+//            Text("Your name is \(user.firstName) \(user.lastName)")
+//
+//            TextField("First name", text: $user.firstName)
+//            TextField("Last name", text: $user.lastName)
+//
+//        }
+//
+//
+//
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
