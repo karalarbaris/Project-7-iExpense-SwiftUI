41619fc6f47d8e62c54da6382a30d91dcd9f5fe6
Baris <karalarbaris@gmail.com>
2021-06-17 20:18:36 +0200

    Initial Commit

---
+++ Project-7-iExpense-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-7-iExpense-SwiftUI
+//
+//  Created by Baris Karalar on 17.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-7-iExpense-SwiftUI/Project_7_iExpense_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_7_iExpense_SwiftUIApp.swift
+//  Project-7-iExpense-SwiftUI
+//
+//  Created by Baris Karalar on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_7_iExpense_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
